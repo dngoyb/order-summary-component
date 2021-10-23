c0b7526931080a9fdb5d92909699eb1cc67e39c6
Ngoy banza <dngoyb@gmail.com>
2021-10-23 21:15:16 +0200

    order-summary-component, using Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				brightblue: 'hsl(245, 75%, 52%)',
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
